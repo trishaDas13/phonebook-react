c0bdb5e26924dc391549aff56af14b2028eb12ac
trishaDas13 <trisha.das1308@gmail.com>
2024-01-18 12:38:19 +0530

    routing done

---
+++ src/copms/AddPerson.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddPerson = () => {
+  return (
+    <div>AddPerson</div>
+  )
+}
+
+export default AddPerson
---
+++ src/copms/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header>
+        <nav>
+            <p>Trisha's Directory App</p>
+        </nav>
+        <div className="btns">
+            <Link to= '/'><button>Add New Person</button></Link>
+            <Link to= 'information'><button>Retrieve Information</button></Link>
+        </div>
+    </header>
+  )
+}
+
+export default Header
